c88b2fb0edb870f2d8573a26d5ffb59166f55069
vasilyl1 <likhovaido@gmail.com>
2023-01-28 22:51:48 -0500

    feature: fetched weather information

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,6 @@
 let userInput = document.querySelector("#btnSearch");
-let geoData = [];
+let geoData = {};
+let weatherData = {};
 
 
 let manageHistoryInput = function (input) {
@@ -11,59 +12,63 @@ let manageHistoryInput = function (input) {
             isInHistory = true;
         }
     };
-        if (! isInHistory) { // the city is not in history
-            for (i = 7; i > 0; i--){ // move the list down
-                searchHistory[i].textContent = searchHistory[i-1].textContent;
-            }
-            searchHistory[0].textContent = input; // save input to the top
+    if (!isInHistory) { // the city is not in history
+        for (i = 7; i > 0; i--) { // move the list down
+            searchHistory[i].textContent = searchHistory[i - 1].textContent;
         }
+        searchHistory[0].textContent = input; // save input to the top
+    }
 };
 
 let geoCode = function (city) { //return geo coordinates by city name
     fetch("http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&appid=" + secret.openWeatherAPI)
-  .then(function (response) {
-    if (response.status === 404) {
-        //document.location.replace(redirectUrl);
-    } else {
-        return response.json();
-    }
-  }). then (function(data){
-   /* geoData = {
-        country: data[0].country, 
-        lat: data[0].lat,
-        lon: data[0].lon,
-        state: data[0].state,
-        name: data[0].name
-    } */
-    geoData[0] = data[0].lat;
-    geoData[1] = data[0].lon;
-    console.log(data[0]);
-    console.log(geoData);
-  });
-    return(geoData);
-};
+        .then(function (response) {
+            if (response.status === 404) { //checks for fetching errors
+                //document.location.replace(redirectUrl);
+            } else {
+                return response.json();
+            }
+        }).then(function (data) {
+            geoData = {
+                country: data[0].country,
+                lat: data[0].lat,
+                lon: data[0].lon,
+                state: data[0].state,
+                name: data[0].name
+            }
 
-let weatherUpdate = function (data1) { // get JSON object and render weather info
-   
-   console.log(geoCode(data1));
-    
-    //console.log(data[0].length + " " + data[0].name + " lat:" + data[0].lat + " lon:" + data[0].lon + " country:" + data[0].country);
+
+            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + geoData.lat + "&lon=" + geoData.lon + "&appid=" + secret.openWeatherAPI)
+                .then(function (response) {
+                    if (response.status === 404) { //checks for fetching errors
+                        //document.location.replace(redirectUrl);
+                    } else {
+                        return response.json();
+                    }
+                }).then(function (data) {
+                    weatherData = data;
+
+                    console.log(data);
+
+                });
+
+        });
+
 };
 
 let buttonClickHandler = function (event) { //search for the entered city name
     let searchInput = document.querySelector("#searchInput");// get the input search
-    let geoData;
+    let geoData1 = {};
 
     event.preventDefault();
 
     manageHistoryInput(searchInput.value); // manage history of requests
-    geoData = weatherUpdate(searchInput.value); // render weather info
-    console.log (geoData);
+    geoCode(searchInput.value); // render weather info
+
 };
-  
+
 
 
 
 
 userInput.addEventListener('click', buttonClickHandler); // listen to the search button click event
-  
