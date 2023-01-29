b11cc9f9509b3ec8a3d325e3077960270f90d851
vasilyl1 <likhovaido@gmail.com>
2023-01-29 14:30:47 -0500

    further additiona

--- assets/js/script.js
+++ assets/js/script.js
@@ -50,16 +50,30 @@ let geoCode = function (city) { //return geo coordinates by city name
                 }).then(function (data) {
                     if (data.cod === "200") { // onenWeather returns 200 code if ok
 
-
+                        //take every 8 records as the forecast is 5 days and 40 records - every 3 hour
                         //updating day0 data
-                        document.querySelector("#day0City").textContent = data.city.name + ", " + data.city.country;
+                        let day = new Date(data.list[0].dt*1000); //take date as object
+                        document.querySelector("#day0City").textContent = data.city.name + ", " + data.city.country + " (" + day.toLocaleDateString() + ")";
                         document.querySelector("#weatherData0").children[0].textContent = "Temp: " + data.list[0].main.temp + " oC";
                         document.querySelector("#weatherData0").children[1].textContent = "Wind: " + data.list[0].wind.speed + " KPH";
                         document.querySelector("#weatherData0").children[2].textContent = "Humidity: " + data.list[0].main.humidity + " %";
-                        document.querySelector(".box4-2").children[0].textContent = "(" + data.list[0].dt_txt + ")";
-                        document.querySelector(".box4-2").children[1].src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
-                        document.querySelector(".box4-2").children[2].textContent = data.list[0].weather[0].description;
-
+                        document.querySelector(".box4-2").children[0].src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+                        document.querySelector(".box4-2").children[1].textContent = data.list[0].weather[0].description;
+                        //updating day1 data
+                        day = new Date(data.list[0].dt*1000);
+                        document.querySelector(".box6").children[0].textContent = day.toLocaleDateString();
+                        //updating day2 data
+                        day = new Date(data.list[8].dt*1000);
+                        document.querySelector(".box7").children[0].textContent = day.toLocaleDateString();
+                        //updating day3 data
+                        day = new Date(data.list[16].dt*1000);
+                        document.querySelector(".box8").children[0].textContent = day.toLocaleDateString();
+                        //updating day4 data
+                        day = new Date(data.list[24].dt*1000);
+                        document.querySelector(".box9").children[0].textContent = day.toLocaleDateString();
+                        //updating day5 data
+                        day = new Date(data.list[32].dt*1000);
+                        document.querySelector(".box10").children[0].textContent = day.toLocaleDateString();
 
                         console.log(data);
                     } else {
