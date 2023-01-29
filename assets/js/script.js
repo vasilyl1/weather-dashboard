47407db4b24094f015696fa62ab480dd5dfb42a3
vasilyl1 <likhovaido@gmail.com>
2023-01-29 12:04:48 -0500

    further enhancements on API update and styling

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,7 +39,7 @@ let geoCode = function (city) { //return geo coordinates by city name
             }
 
 
-            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + geoData.lat + "&lon=" + geoData.lon + "&appid=" + secret.openWeatherAPI)
+            fetch("http://api.openweathermap.org/data/2.5/forecast?lat=" + geoData.lat + "&lon=" + geoData.lon + "&units=metric&appid=" + secret.openWeatherAPI)
                 .then(function (response) { // returns 6 days weather data
                     if (response.status === 404) { //checks for fetching errors
                         //document.location.replace(redirectUrl);
@@ -48,9 +48,23 @@ let geoCode = function (city) { //return geo coordinates by city name
                         return response.json();
                     }
                 }).then(function (data) {
-                    weatherData = data;
+                    if (data.cod === "200") { // onenWeather returns 200 code if ok
 
-                    console.log(data);
+
+                        //updating day0 data
+                        document.querySelector("#day0City").textContent = data.city.name + ", " + data.city.country;
+                        document.querySelector("#weatherData0").children[0].textContent = "Temp: " + data.list[0].main.temp + " oC";
+                        document.querySelector("#weatherData0").children[1].textContent = "Wind: " + data.list[0].wind.speed + " KPH";
+                        document.querySelector("#weatherData0").children[2].textContent = "Humidity: " + data.list[0].main.humidity + " %";
+                        document.querySelector(".box4-2").children[0].textContent = "(" + data.list[0].dt_txt + ")";
+                        document.querySelector(".box4-2").children[1].src = "http://openweathermap.org/img/wn/" + data.list[0].weather[0].icon + "@2x.png";
+                        document.querySelector(".box4-2").children[2].textContent = data.list[0].weather[0].description;
+
+
+                        console.log(data);
+                    } else {
+                        console.log("err on getting weather data");
+                    }
 
                 });
 
